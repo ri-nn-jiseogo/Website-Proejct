4243c9241c05df335c185124a0137e15d671a34d
jiseogo <jiseogo8@gmail.com>
2025-03-16 12:59:17 +0800

    feat: horizantal align

--- project-name/src/components/learning/index.jsx
+++ project-name/src/components/learning/index.jsx
@@ -5,15 +5,27 @@ import { userState } from '../../models/userinfos'
 import { useSetRecoilState, useRecoilValue } from 'recoil';
 
 
-import {getlevel, getUsers} from "../../firebase.js";
+import { getlevel, getUsers } from "../../firebase.js";
+import { Button } from "react-bootstrap";
 
 
 const learning = () => {
 
 
 
-    return(
-       <div></div>
+    return (
+        <div>
+            <div className="TextBox">
+                <div className ="flex-container" class="flex-container">
+                    <h1>Missons</h1>
+                    <button>?</button>
+                </div>
+                <p className="stage-desc">In the game, you are allowed to participate in up to 15 missions at a time. <br /> If you wish to join more missions, you must wait for a cooldown period before you can continue.</p>
+            </div>
+            <div>
+
+            </div>
+        </div>
     )
 }
 
