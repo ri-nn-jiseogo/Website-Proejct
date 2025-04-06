07a6ab1547844e2e71e6cd2a3c86a37d8664b5da
jiseogo <jiseogo8@gmail.com>
2025-04-06 11:24:57 +0800

    feat: update

---
+++ project-name/Frontend/src/components/lesson1/index.jsx
@@ -0,0 +1,19 @@
+
+import "./lesson1.css";
+
+const lesson1 = () => {
+    return (
+        <div>
+            <div clssName="content">
+                <div className="TextBox">
+                    <div className="flex-container">
+                        <h1 className="missons">Missons</h1>
+                    </div>
+                    <p className="stage-desc">In the game, you are allowed to participate in up to 15 missions at a time. <br /> If you wish to join more missions, you must wait for a cooldown period before you can continue.</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default lesson1;
