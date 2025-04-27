d9801389d674e675120dc1888cb28aa36f41a8b1
jiseogo <jiseogo8@gmail.com>
2025-04-27 20:28:52 +0800

    feat: add sidebar component

--- project-name/Frontend/src/components/Mypage/index.jsx
+++ project-name/Frontend/src/components/Mypage/index.jsx
@@ -4,18 +4,31 @@ import "../Mypage/mypage.css";
 
 const mypage = () => {
 
-    return(
+    return (
         <div className="mypage">
             <div className="content">
-            <div className="TextBox">
-                <div className="flex-container">
-                    <h1 className="missons">My Page</h1>
+                <div className="TextBox">
+                    <div className="flex-container">
+                        <h1 className="missons">My Page</h1>
+                    </div>
+                </div>
+                <div className="info-container">
+                    <h1 className="mission-title">Welcome, Benjamin Oh</h1>
+                    <div className="info">
+                        <div className="profile">
+                            <div className="flex-box">
+                                <h1 className="section-name">Profile</h1>
+                                <button className="Profile-button">Edit</button>
+                            </div>
+                            <div className="profile-info">
+                                <h1 className="section-name">Email: </h1>
+                                <h1 className="section-name">Name: </h1>
+                                <h1 className="section-name">Grade: </h1>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="info-container">
-                    <h1 className="mission-title">Lesson Mission History</h1>
-                </div>
-        </div>
         </div>
     )
 }
