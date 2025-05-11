ed969cd966b2513aea94c64e6c524c893c4a1340
jiseogo <jiseogo8@gmail.com>
2025-05-11 19:04:21 +0800

    feat: add new features and refactor code

--- project-name/Frontend/src/components/sidebar/index.jsx
+++ project-name/Frontend/src/components/sidebar/index.jsx
@@ -3,11 +3,17 @@ import { Link } from "react-router-dom"
 import { Form, Button, Alert } from "react-bootstrap";
 
 import Logo from "../../assets/logo.png";
+import { useRecoilValue } from 'recoil';
+import { userState } from "../../models/userinfos/index.js";
+
 
 
 
 const Sidebar = () => {
+
+    const user = useRecoilValue(userState);
     return (
+
         <div className="Sidebar">
             <div className="sidebar-wrapper">
                 <ul className="side-link">
@@ -17,6 +23,11 @@ const Sidebar = () => {
                     <li><Link to="/user/challenges">Challenges</Link></li>
                     <li><Link to="/user/resources">Learning Resources</Link></li>
                     <li><Link to="/user/mypage">My Page</Link></li>
+                    {user.isstaff && (
+                        <li>
+                            <Link to="/user/admin">Admin</Link>
+                        </li>
+                    )}
                 </ul>
                 <Button className="w-100" variant="primary" type="submit" href="/login">
                     Log Out
